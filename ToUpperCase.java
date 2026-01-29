548480f440de677eb13eca9e1fa402c2440f3c2f
KevinEduardo-1 <santiagocanaleskevin@gmail.com>
2026-01-28 22:55:46 -0500

    first commit

---
+++ ToUpperCase.java
@@ -0,0 +1,14 @@
+public class ToUpperCase {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            String input = args[0];
+            String output = input.toUpperCase();
+            System.out.println(output);
+
+        }else{
+            System.out.println("Por favor ingrese un argumento");
+        }
+    }
+}
+        
+
